b1d5667b5b58245ff89e389d4810104974723104
ddrkt <toxa.ship@gmail.com>
2019-11-03 01:50:44 +0300

    fix: clear comments

--- lib/vector/vector.rb
+++ lib/vector/vector.rb
@@ -24,15 +24,7 @@ module FastMatrix
     def to_s
       "#{self.class}[#{to_ary.join(', ')}]"
     end
-
-    # def to_str
-    #   to_s
-    # end
-
-    # def inspect
-    #   to_s
-    # end
-
+    
     def each_with_index
       (0...size).each do |i|
         yield self[i], i
